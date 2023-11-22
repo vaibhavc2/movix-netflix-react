f75aa18c520ebed80e8feabacca08470d39dfe88
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-11-22 21:15:11 +0530

    fix some issues and code

--- src/app/router/pages/home/Home.tsx
+++ src/app/router/pages/home/Home.tsx
@@ -1,50 +1,11 @@
-import { tmdbAPI } from "@/api/tmdb.api";
-import { setApiConfig, setGenres } from "@/store/reducers/home-slice";
-import { useAppDispatch } from "@/store/store";
 import "@/styles/scss/other/pages/home.scss";
-import { memo, useCallback, useEffect } from "react";
+import { memo } from "react";
 import HeroBanner from "./hero/HeroBanner";
 import Popular from "./popular/Popular";
 import TopRated from "./top-rated/TopRated";
 import Trending from "./trending/Trending";
 
 const Home = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    fetchApiConfig();
-    genresCall();
-  }, []);
-
-  const fetchApiConfig = useCallback(() => {
-    tmdbAPI.fetchData("/configuration").then((res: any) => {
-      const url = {
-        backdrop: res?.images?.secure_base_url + "original",
-        poster: res?.images?.secure_base_url + "original",
-        profile: res?.images?.secure_base_url + "original",
-      };
-
-      dispatch(setApiConfig(url));
-    });
-  }, [tmdbAPI.fetchData, setApiConfig, dispatch]);
-
-  const genresCall = useCallback(async () => {
-    let promises = Array<Promise<any>>();
-    let endPoints = ["tv", "movie"];
-    const allGenres: any = {};
-
-    endPoints.forEach((url) => {
-      promises.push(tmdbAPI.fetchData(`/genre/${url}/list`));
-    });
-
-    const data: any = await Promise.all(promises);
-    data.map(({ genres }: any) => {
-      return genres.map((item: any) => (allGenres[item.id] = item));
-    });
-
-    dispatch(setGenres(allGenres));
-  }, [tmdbAPI.fetchData, setGenres, dispatch]);
-
   return (
     <div className="homePage">
       <HeroBanner />
