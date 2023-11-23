0416629ef809f6b0d61251fdf8354e01bcab3bde
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-11-23 23:26:48 +0530

    page not found

--- src/app/router/pages/PageNotFound.tsx
+++ src/app/router/pages/PageNotFound.tsx
@@ -1,12 +1,18 @@
+import ContentWrapper from "@/components/ContentWrapper";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+
+import "@/styles/scss/other/pages/page-not-found.scss";
 
 const PageNotFound = () => {
   return (
     <div className="flex h-screen flex-col justify-between">
       <Header />
-      <div className="m-8 flex h-[90vh] flex-col items-center justify-center text-center font-mono text-lg text-slate-50">
-        Page Not Found! Error: 404
+      <div className="pageNotFound m-8 flex h-[90vh] flex-col items-center justify-center text-center font-mono">
+        <ContentWrapper>
+          <div className="bigText">Error: 404</div>
+          <div className="smallText">Page Not Found!</div>
+        </ContentWrapper>
       </div>
       <Footer />
     </div>
